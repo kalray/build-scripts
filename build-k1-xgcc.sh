2cbaf07fcae8d6126055d06ae58b448a51cd54d0
Marc Poulhiès <marc.poulhies@kalray.eu>
2019-10-31 14:52:00 +0100

    Update build script and README.md
    Backport script and README.md changes from release branch.

--- build-k1-xgcc.sh
+++ build-k1-xgcc.sh
@@ -4,45 +4,47 @@ set -eu
 DOWNLOAD_REQ=1
 
 ## Set these for using specific revision
-SHA1_GCC=
-SHA1_GDB=
-SHA1_NEWLIB=
+SHA1_GCC=${SHA1_GCC:-HEAD}
+SHA1_GDB=${SHA1_GDB:-HEAD}
+SHA1_NEWLIB=${SHA1_NEWLIB:-HEAD}
 
 TARGET=k1-elf
 PREFIX=$(realpath $1)
 
 PARALLEL_JOBS=-j6
 
-# Used to redirect some output
 
 mkdir -p $PREFIX
 export PATH=$PREFIX/bin:$PATH
 
-git clone --depth 1 -b coolidge https://github.com/kalray/gdb-binutils.git
-if [[ ! -z "$SHA1_GDB" ]]
-then
-   cd gdb-binutils
-   git reset --hard $SHA1_GDB
-   cd -
-fi
+function git_clone() {
+    local repo=$1
+    local sha1=$2
 
-git clone --depth 1 -b coolidge https://github.com/kalray/newlib.git
-if [[ ! -z "$SHA1_NEWLIB" ]]
-then
-   cd newlib
-   git reset --hard $SHA1_NEWLIB
-   cd -
-fi
+    repo_dir=$(basename ${repo} ".git")
+    echo "Cloning ${repo} (${repo_dir}) sha1: ${sha1}"
+    if [ -d ${repo_dir} ]; then
+	cd ${repo_dir}
+	git fetch
+	cd -
+    else
+	git clone --depth 1 -b coolidge ${repo}
+    fi
 
-git clone --depth 1 -b coolidge https://github.com/kalray/gcc.git
-if [[ ! -z "$SHA1_GCC" ]]
-then
-   cd gcc
-   git reset --hard $SHA1_GCC
-   cd -
-fi
+    if [[ ! -z "${sha1}" ]]
+    then
+	cd ${repo_dir}
+	git reset --hard ${sha1}
+	cd -
+    fi
+}
 
-mkdir build-binutils
+git_clone https://github.com/kalray/gdb-binutils.git ${SHA1_GDB}
+git_clone https://github.com/kalray/newlib.git ${SHA1_NEWLIB}
+git_clone https://github.com/kalray/gcc.git ${SHA1_GCC}
+
+
+mkdir -p build-binutils
 cd build-binutils
 ../gdb-binutils/configure \
     --prefix=$PREFIX \
@@ -61,41 +63,45 @@ make install
 cd -
 cd gcc
 
+## This is used only when distribution does not have correct dependencies.
 ##./contrib/download_prerequisites
 
 cd -
-mkdir build-gcc
+mkdir -p build-gcc
 cd build-gcc
-../gcc/configure --prefix=$PREFIX \
-		 --target=$TARGET \
-		 --with-gnu-as \
-		 --with-gnu-ld \
-		 --disable-bootstrap \
-		 --disable-shared \
-		 --enable-multilib \
-		 --disable-libmudflap \
-		 --disable-libssp \
-		 --enable-__cxa_atexit \
-		 --with-bugurl=no \
-		 --with-newlib                      \
-		 --disable-libgomp \
-		 --disable-libatomic \
-		 --disable-threads \
-		 --enable-languages=c \
-		 --with-system-zlib
+../gcc/configure \
+    --prefix=$PREFIX \
+    --target=$TARGET  \
+    --with-gnu-as \
+    --with-gnu-ld \
+    --disable-bootstrap \
+    --disable-shared \
+    --enable-multilib \
+    --disable-libmudflap \
+    --disable-libssp \
+    --enable-__cxa_atexit \
+    --with-bugurl=no \
+    --with-newlib                      \
+    --disable-libgomp \
+    --disable-libatomic \
+    --disable-threads \
+    --enable-languages=c,c++ \
+    --with-system-zlib
 
 make all-gcc $PARALLEL_JOBS > /dev/null
 make install-gcc
 
 cd -
-mkdir build-newlib
+mkdir -p build-newlib
 cd  build-newlib
-../newlib/configure --target=$TARGET --prefix=$PREFIX \
-            --enable-multilib \
-            --enable-target-optspace \
-            --enable-newlib-io-c99-formats \
-            --target=${TARGET} \
-	    --enable-newlib-multithread
+../newlib/configure \
+    --target=$TARGET \
+    --prefix=$PREFIX \
+    --enable-multilib \
+    --enable-target-optspace \
+    --enable-newlib-io-c99-formats \
+    --target=${TARGET} \
+    --enable-newlib-multithread
 
 make all $PARALLEL_JOBS > /dev/null
 make install
