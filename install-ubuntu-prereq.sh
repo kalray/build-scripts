263bf2ac63f9aee312ab79e77ac45e9bea172197
Laurent Thévenoux <lrnt@thvnx.com>
2021-05-18 18:54:51 +0200

    Build LLVM toolchain from GitHub sources
    This patch allows to build LLVM based on the ClusterOS port available
    at https://github.com/kalray/llvm-project.
    
    It only builds llvm-project (clang, opt, llc, ...) based on Kalray's
    port (based on ClusterOS). This toolchain is *not* able to produce
    executable code since no binutils, libm/libc, nor compiler-rt are
    provided but llvm IR and KVX asm only.

--- install-ubuntu-prereq.sh
+++ install-ubuntu-prereq.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -eu
 
-sudo apt-get install libgmp-dev libmpfr-dev libmpc-dev texinfo zlib1g-dev bison flex libtool
+sudo apt-get install libgmp-dev libmpfr-dev libmpc-dev texinfo zlib1g-dev bison flex libtool ninja-build
